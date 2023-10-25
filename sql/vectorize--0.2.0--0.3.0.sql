99f426d452e829df22fe3f7849aa56be372e7864
Adam Hendel <15756360+ChuckHend@users.noreply.github.com>
2023-10-24 23:48:51 -0500

    add update sql

---
+++ sql/vectorize--0.2.0--0.3.0.sql
@@ -0,0 +1,33 @@
+DROP FUNCTION vectorize."table";
+
+CREATE  FUNCTION vectorize."table"(
+        "table" TEXT, /* &str */
+        "columns" TEXT[], /* alloc::vec::Vec<alloc::string::String> */
+        "job_name" TEXT, /* core::option::Option<alloc::string::String> */
+        "args" json, /* pgrx::datum::json::Json */
+        "primary_key" TEXT, /* alloc::string::String */
+        "schema" TEXT DEFAULT 'public', /* alloc::string::String */
+        "update_col" TEXT DEFAULT 'last_updated_at', /* alloc::string::String */
+        "transformer" vectorize.Transformer DEFAULT 'openai', /* vectorize::types::Transformer */
+        "search_alg" vectorize.SimilarityAlg DEFAULT 'pgv_cosine_similarity', /* vectorize::types::SimilarityAlg */
+        "table_method" vectorize.TableMethod DEFAULT 'append', /* vectorize::init::TableMethod */
+        "schedule" TEXT DEFAULT '* * * * *' /* alloc::string::String */
+) RETURNS TEXT /* core::result::Result<alloc::string::String, anyhow::Error> */
+LANGUAGE c /* Rust */
+
+AS 'MODULE_PATHNAME', 'table_wrapper';
+
+DROP FUNCTION vectorize."search";
+
+CREATE  FUNCTION vectorize."search"(
+        "job_name" TEXT, /* &str */
+        "query" TEXT, /* &str */
+        "api_key" TEXT, /* &str */
+        "return_columns" TEXT[] DEFAULT ARRAY['*']::text[], /* alloc::vec::Vec<alloc::string::String> */
+        "num_results" INT DEFAULT 10 /* i32 */
+) RETURNS TABLE (
+        "search_results" jsonb  /* pgrx::datum::json::JsonB */
+)
+STRICT
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'search_wrapper';
