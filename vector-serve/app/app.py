f59f13a41f33a12f5fa0d3e57b0ef495923f8863
Adam Hendel <ChuckHend@users.noreply.github.com>
2024-01-29 09:10:05 -0600

    merge vector-serve (#39)
    merge vector-serve project into repo
    
    ---------
    
    Co-authored-by: Adam Hendel <15756360+ChuckHend@users.noreply.github.com>
    Co-authored-by: Russell Kemmit <russell.kemmit@shipt.com>
    Co-authored-by: russellkemmit <99747292+russellkemmit@users.noreply.github.com>

---
+++ vector-serve/app/app.py
@@ -0,0 +1,40 @@
+from typing import Callable
+import logging
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routes.transform import router as transform_router
+from app.routes.info import router as info_router
+from app.models import load_model_cache
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = FastAPI(title="Tembo-Embedding-Service")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(transform_router)
+app.include_router(info_router)
+
+
+def start_app_handler(app: FastAPI) -> Callable:
+    def startup() -> None:
+        logging.info("Running app start handler.")
+        load_model_cache(app)
+
+    return startup
+
+
+app.add_event_handler("startup", start_app_handler(app))
+
+if __name__ == "__main__":
+    import uvicorn  # type: ignore
+
+    uvicorn.run("src.app:app", host="0.0.0.0", port=5000, reload=True)
---
+++ vector-serve/app/routes/transform.py
@@ -0,0 +1,79 @@
+import logging
+import os
+from typing import TYPE_CHECKING, Any, List
+
+from app.models import model_org_name, get_model
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel, conlist
+
+router = APIRouter(tags=["transform"])
+
+logging.basicConfig(level=logging.DEBUG)
+
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", 1000))
+
+
+if TYPE_CHECKING:
+    Vector = List[str]
+else:
+    Vector = conlist(str, min_length=1)
+
+
+class Batch(BaseModel):
+    input: Vector
+    model: str = "all-MiniLM-L12-v2"
+    normalize: bool = False
+
+
+class Embedding(BaseModel):
+    embedding: list[float]
+    index: int
+
+
+class ResponseModel(BaseModel):
+    data: list[Embedding]
+    model: str
+
+
+@router.post("/v1/embeddings", response_model=ResponseModel)
+def batch_transform(request: Request, payload: Batch) -> ResponseModel:
+    logging.info({"batch-predict-len": len(payload.input)})
+    batches = chunk_list(payload.input, BATCH_SIZE)
+    num_batches = len(batches)
+    responses: list[list[float]] = []
+
+    requested_model = model_org_name(payload.model)
+
+    try:
+        model = get_model(
+            model_name=requested_model, model_cache=request.app.state.model_cache
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Unable to load {payload.model} -- {e}",
+        )
+
+    for idx, batch in enumerate(batches):
+        logging.info(f"Batch {idx} / {num_batches}")
+        responses.extend(
+            model.encode(
+                sentences=batch, normalize_embeddings=payload.normalize
+            ).tolist()
+        )
+    logging.info("Completed %s batches", num_batches)
+    embeds = [
+        Embedding(embedding=embedding, index=i) for i, embedding in enumerate(responses)
+    ]
+    return ResponseModel(
+        data=embeds,
+        model=requested_model,
+    )
+
+
+def chunk_list(lst: List[Any], chunk_size: int) -> List[List[Any]]:
+    """Split a list into smaller lists of equal length, except the last one."""
+    chunks = []
+    for i in range(0, len(lst), chunk_size):
+        chunks.append(lst[i : i + chunk_size])
+    return chunks
