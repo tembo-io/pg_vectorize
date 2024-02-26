d6a274df0fc13235d97136f75f2e048b4f048ed8
Adam Hendel <hendel.adam@gmail.com>
2024-02-26 17:01:15 -0600

    add tests

---
+++ vector-serve/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from starlette.testclient import TestClient
+
+from app.app import app
+
+@pytest.fixture()
+def test_client():
+    with TestClient(app) as test_client:
+        yield test_client
