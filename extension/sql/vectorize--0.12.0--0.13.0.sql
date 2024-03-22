7c390f97810aa163c4795e1838d0fed3ad4b822c
Adam Hendel <hendel.adam@gmail.com>
2024-03-21 19:29:41 -0500

    update migration

--- extension/sql/vectorize--0.12.0--0.13.0.sql
+++ extension/sql/vectorize--0.12.0--0.13.0.sql
@@ -8,7 +8,7 @@ CREATE  FUNCTION vectorize."search"(
 	"api_key" TEXT DEFAULT NULL, /* core::option::Option<alloc::string::String> */
 	"return_columns" TEXT[] DEFAULT ARRAY['*']::text[], /* alloc::vec::Vec<alloc::string::String> */
 	"num_results" INT DEFAULT 10, /* i32 */
-	"filter" TEXT[] DEFAULT NULL /* core::option::Option<alloc::vec::Vec<alloc::string::String>> */
+	"where_sql" TEXT DEFAULT NULL /* core::option::Option<alloc::string::String> */
 ) RETURNS TABLE (
 	"search_results" jsonb  /* pgrx::datum::json::JsonB */
 )
