11a79649eef91ba25062a52d89eb2c25c82c7f9c
Evan Stanton <evan.h.stanton@gmail.com>
2024-04-03 22:53:10 +0200

    Update 13-14 sql file.
    Signed-off-by: Evan Stanton <evan.h.stanton@gmail.com>

--- extension/sql/vectorize--0.13.0--0.14.0.sql
+++ extension/sql/vectorize--0.13.0--0.14.0.sql
@@ -1,2 +1,18 @@
-ALTER TABLE vectorize.job ADD COLUMN index_dist_type TEXT NOT NULL DEFAULT 'pgv_hsnw_cosin';
-
+ DROP FUNCTION IF EXISTS vectorize."table"(TEXT, TEXT[], TEXT, TEXT, json, TEXT, TEXT, TEXT, vectorize.SimilarityAlg, vectorize.TableMethod, TEXT); 
+	 CREATE  FUNCTION vectorize."table"( 
+	         "table" TEXT, /* &str */ 
+	         "columns" TEXT[], /* alloc::vec::Vec<alloc::string::String> */ 
+	         "job_name" TEXT, /* alloc::string::String */ 
+	         "primary_key" TEXT, /* alloc::string::String */ 
+	         "args" json DEFAULT '{}', /* pgrx::datum::json::Json */ 
+	         "schema" TEXT DEFAULT 'public', /* alloc::string::String */ 
+	         "update_col" TEXT DEFAULT 'last_updated_at', /* alloc::string::String */ 
+                 "index_dist_type" TEXT DEFAULT 'pgv_hsnw_cosine', /* vectorize::types::IndexDist */
+	         "transformer" TEXT DEFAULT 'text-embedding-ada-002', /* alloc::string::String */ 
+	         "search_alg" vectorize.SimilarityAlg DEFAULT 'pgv_cosine_similarity', /* vectorize::types::SimilarityAlg */ 
+	         "table_method" vectorize.TableMethod DEFAULT 'append', /* vectorize::types::TableMethod */ 
+	         "schedule" TEXT DEFAULT 'realtime' /* alloc::string::String */ 
+	 ) RETURNS TEXT /* core::result::Result<alloc::string::String, anyhow::Error> */ 
+	 STRICT 
+	 LANGUAGE c /* Rust */ 
+	 AS 'MODULE_PATHNAME', 'table_wrapper';
