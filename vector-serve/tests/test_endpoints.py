be7b087698e289875d429954ecac3a5da5cd0429
Adam Hendel <hendel.adam@gmail.com>
2024-05-23 13:41:06 +0000

    add test

--- vector-serve/tests/test_endpoints.py
+++ vector-serve/tests/test_endpoints.py
@@ -14,3 +14,9 @@ def test_alive_endpoint(test_client):
 def test_model_info(test_client):
     response = test_client.get("/v1/info", params={"model_name": "all-MiniLM-L12-v2"})
     assert response.status_code == 200
+
+
+def test_metrics_endpoint(test_client):
+    response = test_client.get("/metrics")
+    assert response.status_code == 200
+    assert "all-MiniLM-L12-v2" in response.text
