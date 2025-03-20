8f5fb6fa1298f8cdac3fbb6c7a30bc693abd650f
Adam Hendel <ChuckHend@users.noreply.github.com>
2025-03-20 11:56:09 -0500

    update sql for param change (#213)
    * update sql for param change
    
    * docs update

--- extension/sql/vectorize--0.21.1--0.22.0.sql
+++ extension/sql/vectorize--0.21.1--0.22.0.sql
@@ -1,3 +1,46 @@
+DROP FUNCTION vectorize."table";
+-- src/api.rs:94
+-- vectorize::api::table
+CREATE  FUNCTION vectorize."table"(
+	"relation" TEXT, /* &str */
+	"columns" TEXT[], /* alloc::vec::Vec<alloc::string::String> */
+	"job_name" TEXT, /* &str */
+	"primary_key" TEXT, /* &str */
+	"schema" TEXT DEFAULT 'public', /* &str */
+	"update_col" TEXT DEFAULT 'last_updated_at', /* alloc::string::String */
+	"index_dist_type" IndexDist DEFAULT 'pgv_hnsw_cosine', /* vectorize::types::IndexDist */
+	"transformer" TEXT DEFAULT 'sentence-transformers/all-MiniLM-L6-v2', /* &str */
+	"table_method" TableMethod DEFAULT 'join', /* vectorize::types::TableMethod */
+	"schedule" TEXT DEFAULT '* * * * *' /* &str */
+) RETURNS TEXT /* core::result::Result<alloc::string::String, anyhow::Error> */
+STRICT
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'table_wrapper';
+/* </end connected objects> */
+
+DROP FUNCTION vectorize."table_from";
+-- src/api.rs:380
+-- vectorize::api::table_from
+CREATE  FUNCTION vectorize."table_from"(
+	"relation" TEXT, /* &str */
+	"columns" TEXT[], /* alloc::vec::Vec<alloc::string::String> */
+	"job_name" TEXT, /* &str */
+	"primary_key" TEXT, /* &str */
+	"src_table" TEXT, /* &str */
+	"src_primary_key" TEXT, /* &str */
+	"src_embeddings_col" TEXT, /* &str */
+	"schema" TEXT DEFAULT 'public', /* &str */
+	"update_col" TEXT DEFAULT 'last_updated_at', /* alloc::string::String */
+	"index_dist_type" IndexDist DEFAULT 'pgv_hnsw_cosine', /* vectorize::types::IndexDist */
+	"transformer" TEXT DEFAULT 'sentence-transformers/all-MiniLM-L6-v2', /* &str */
+	"table_method" TableMethod DEFAULT 'join', /* vectorize::types::TableMethod */
+	"schedule" TEXT DEFAULT '* * * * *' /* &str */
+) RETURNS TEXT /* core::result::Result<alloc::string::String, anyhow::Error> */
+STRICT
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'table_from_wrapper';
+
+
 -- Rename 'table' key to 'relation' in the params JSONB column
 UPDATE vectorize.job
 SET params = jsonb_set(
@@ -9,7 +52,8 @@ SET params = jsonb_set(
 WHERE params ? 'table';
 
 -- Update the function to reference "relation" instead of "table"
-CREATE OR REPLACE FUNCTION handle_table_drop()
+DROP FUNCTION vectorize."handle_table_drop";
+CREATE FUNCTION vectorize.handle_table_drop()
 RETURNS event_trigger AS $$
 DECLARE
     obj RECORD;
